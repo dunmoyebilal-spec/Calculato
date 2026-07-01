7e67c944cbeedd3829d95c14346ad7036783ff9b
dunmoyebilal-spec <dunmoyebilal@gmail.com>
2026-07-01 08:20:26 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+// CALCULATOR SCRIPT
+
+const display = document.getElementById('display');
+
+function appendToDisplay(input) {
+    display.value += input;
+}
+
+function appendOperator(input) {
+    const operators = ['+', '-', '*', '/'];
+    const lastchar = display.value.slice(-1);
+
+    if (operators.includes(lastchar)) {
+        display.value = display.value.slice(0, -1) + input;
+    } else {
+        display.value += input;
+    }
+}
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
